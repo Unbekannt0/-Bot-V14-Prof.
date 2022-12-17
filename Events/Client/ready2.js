62b4ba8ba882829bbe64a1125398a89e326a33cd
Ashrlay <99417737+Unbekannt0@users.noreply.github.com>
2022-12-17 11:35:24 +0100

    Delete Events/Client directory

--- Events/Client/ready2.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "ready",
-    once: true,
-	async execute(client) {
-console.log(`Logged to the client ${client.user.username}\n ${client.guilds.cache.size}`.blue);
-	}};
